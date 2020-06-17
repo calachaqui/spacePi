e585b5ff7b5ca9980c9587a60cda28a6ff79d4f5
User <user@Mac-2.local>
2020-06-16 22:58:30 -0600

    start work on LoRa send/receive protocal

---
+++ LoRa/database_setup.py
@@ -0,0 +1,41 @@
+import sqlite3
+from sqlite3 import Error as e
+
+# connect to datebase
+conn = sqlite3.connect('rocket_data.db')
+c = conn.cursor()
+
+sql_cmd = """CREATE TABLE outgoing_message (
+q_timestamp datetime,
+sender_id VARCHAR(24),
+message CHAR(255),
+send_status CHAR(12),
+send_timestamp datetime,
+PRIMARY KEY (q_timestamp)
+)"""
+
+c.execute(sql_cmd)
+
+sql_cmd = """CREATE TABLE incoming_message (
+msg_timestamp datetime,
+sender_id VARCHAR(24),
+message CHAR(255),
+exec_status CHAR(12),
+exec_timestamp datetime,
+PRIMARY KEY (msg_timestamp)
+)"""
+
+c.execute(sql_cmd)
+
+sql_cmd = """CREATE TABLE rocket_data (
+rcd_timestamp datetime,
+altitude FLOAT,
+temp FLOAT,
+gps_coord CHAR(50),
+PRIMARY KEY (rcd_timestamp)
+)"""
+
+c.execute(sql_cmd)
+
+conn.commit()
+print("database and tables are created")
---
+++ LoRa/send_receive_protocol.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sqlite3
+import datetime
+import time
+
+#start up the lora radio
+
+#listen for incoming messages
+
+#store message if any received
+
+#send next messagge in database queue table
+
+#update message record in queue table
+
+#sleep short time
+
+
